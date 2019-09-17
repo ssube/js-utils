659147089b6e3fa2a148bafd9a939c8972e22cf2
ssube <seansube@gmail.com>
2019-09-17 07:47:12 -0500

    fix(test): spy on global console log

--- test/TestApp.ts
+++ test/TestApp.ts
@@ -1,12 +1,13 @@
 import { expect } from 'chai';
 import { spy } from 'sinon';
 
-import { describeLeaks, itLeaks } from './helpers/async';
 import { createApp } from '../src/app';
+import { describeLeaks, itLeaks } from './helpers/async';
 
 describeLeaks('app', async () => {
   itLeaks('should log a message', async () => {
-    const logSpy = spy(console.log);
+    /* tslint:disable-next-line:no-console no-unbound-method */
+    const logSpy = spy(console, 'log');
 
     await createApp();
 
