49ca0856e7605d0b6d19a2d46e398aeeaae9bb96
ssube <seansube@gmail.com>
2019-09-21 05:32:44 -0500

    fix(scripts): escape docker tags

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,14 +1,17 @@
-#! /bin/sh
+#! /bin/bash
 
 IMAGE_PUSH="${1:---skip}"
-IMAGE_TAG="${CI_PROJECT_PATH}:${CI_COMMIT_TAG:-${CI_COMMIT_REF_SLUG}}"
 
-echo "Building image: ${IMAGE_TAG}"
+IMAGE_NAME="${CI_PROJECT_PATH}"
+IMAGE_TAG="$(echo "${CI_COMMIT_TAG:-${CI_COMMIT_REF_SLUG}}" | sed -r 's/[^-_a-zA-Z0-9]/-/g')"
+IMAGE_FULL="${IMAGE_NAME}:${IMAGE_TAG}"
 
-docker build -t ${IMAGE_TAG} .
+echo "Building image: ${IMAGE_FULL}"
+
+docker build -t ${IMAGE_FULL} .
 
 if [[ "${IMAGE_PUSH}" == "--push" ]];
 then
-  echo "Pushing image: ${IMAGE_TAG}"
-  docker push ${IMAGE_TAG}
+  echo "Pushing image: ${IMAGE_FULL}"
+  docker push ${IMAGE_FULL}
 fi
