aae0412bf6d6ce4f096c5784317874321ade7d9d
ssube <seansube@gmail.com>
2019-09-21 04:38:46 -0500

    feat(scripts): copy some files into new projects only once

--- scripts/project-update.sh
+++ scripts/project-update.sh
@@ -7,4 +7,10 @@ echo "Updating project: ${PROJECT_NAME}"
 git ls-tree -r HEAD --name-only | rsync -avh \
   --files-from=- \
   --exclude-from=./scripts/project-exclude \
+  --exclude-from=./scripts/project-newonly \
+  ./ "${PROJECT_NAME}"
+
+rsync -avh \
+  --files-from=./scripts/project-newonly \
+  --ignore-existing \
   ./ "${PROJECT_NAME}"
