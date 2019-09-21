d3ccff9545575cb9f22edb70b1a8c5177a80bb57
ssube <seansube@gmail.com>
2019-09-21 05:05:51 -0500

    fix(build): move docker commands to script, detect project path outside of CI

---
+++ scripts/docker-build.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+IMAGE_PUSH="${1:---skip}"
+IMAGE_TAG="${CI_PROJECT_PATH}:${CI_COMMIT_TAG:-${CI_COMMIT_REF_SLUG}}"
+
+echo "Building image: ${IMAGE_TAG}"
+
+docker build -t ${IMAGE_TAG} .
+
+if [[ "${IMAGE_PUSH}" == "--push" ]];
+then
+  echo "Pushing image: ${IMAGE_TAG}"
+  docker push ${IMAGE_TAG}
+fi
