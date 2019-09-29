49d5c73a976317a4893567ad24b7116ecbcece77
ssube <seansube@gmail.com>
2019-09-29 16:06:03 -0500

    feat(build): externalize rollup data

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-import { VERSION_INFO } from './version';
-
+/* tslint:disable:no-default-import */
 import JSON_DATA from './resource/json.json';
 import YAML_DATA from './resource/yaml.yml';
+import { VERSION_INFO } from './version';
 
 export async function main(argv: Array<string>): Promise<number> {
   // tslint:disable-next-line:no-console
