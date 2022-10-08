92305327429b1f128b95ba2a52b4f0d28b5f5858
Sean Sube <seansube@gmail.com>
2022-10-08 15:17:38 -0500

    fix(build): add target to upload coverage to codeclimate

--- config/project.mk
+++ config/project.mk
@@ -1,1 +1,5 @@
 ci-full: ci
+
+upload-climate:
+	cc-test-reporter format-coverage -t lcov -o $(TARGET_PATH)/coverage/codeclimate.json -p $(ROOT_PATH) $(TARGET_PATH)/coverage/lcov.info
+	cc-test-reporter upload-coverage --debug -i $(TARGET_PATH)/coverage/codeclimate.json -r "$(shell echo "${CODECLIMATE_SECRET}" | base64 -d)"
